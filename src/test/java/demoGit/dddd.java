407340b72d19f52de59da27c2450eff9c19274ce
CDIT BIDAR <CDIT BIDAR@LAPTOP-I6K0391G>
2022-01-24 20:06:05 +0530

    2nd class

---
+++ src/test/java/demoGit/dddd.java
@@ -0,0 +1,13 @@
+package demoGit;
+
+import org.testng.annotations.Test;
+
+public class dddd {
+	@Test
+	public void demogit(){
+
+		System.out.println("Hi this is user1 FIRST commit");
+		System.out.println("Hi this is user1 FIRST commit");
+	}
+
+}
